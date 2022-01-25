92bd98ba134ab8e1eb74acde1ad98e61a901ed93
han <63923803+stuckinatimezone@users.noreply.github.com>
2022-01-25 20:11:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from random import choice
+import discord
+from discord.ext import commands, tasks
+from music import Player
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="&", intents=intents)
+
+status = ['Music', 'Mario', 'Sleeping', 'poptropica', 'fantage', 'Heat waves', 'I love you han', 'with a million lives', 'shes cute', 'I am in love', 'Rocket league', 'Detroit become human', 'Resting', 'Alive', 'with han',]
+
+@bot.event
+async def on_ready():
+    change_status.start()
+    print(f"{bot.user.name} is up and running.")
+
+async def setup():
+    await bot.wait_until_ready()
+    bot.add_cog(Player(bot))
+
+@tasks.loop(seconds=30)
+async def change_status():
+    await bot.change_presence(activity=discord.Game(choice(status)))
+
+bot.loop.create_task(setup())
+
+bot.run("OTA5ODIwOTk1MjQ0NzkzOTU5.YZJ21A.cORqlnuG_x2ytF1ILA9DRxOswKE")
